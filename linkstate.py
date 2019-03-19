9dc8dd793e62218c20a3554d0a3c00e053b7d270
Kadie Clancy <kadieclancy@Kadies-MBP.fios-router.home>
2019-03-19 18:20:16 -0400

    link state obj

--- linkstate.py
+++ linkstate.py
@@ -1,1 +1,14 @@
+# linkstate.py
+# contains the Link State info for a local link state
 
+class LinkState:
+	def _init_(self, IP):
+		self.IP = IP
+		self.mapping = {0: self.IP}
+		self.LSDB = {0: 0}
+
+	def addNeighbor(self, neigh_IP):
+		self.mapping[len(self.mapping)+1] = neigh_IP
+	
+	def updateNeighborDelay(self, neigh_index, delay)
+		self.mapping[neigh_index] = delay
