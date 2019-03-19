ce8a3b91f60fca03f6b34a0f055139acc5fc33db
Kadie Clancy <kadieclancy@Kadies-MBP.fios-router.home>
2019-03-18 21:21:34 -0400

    added user input message and ability for multiple clients

--- client.py
+++ client.py
@@ -2,53 +2,33 @@ import time, threading
 import socket
 from Packet import packet
 import pickle 
+import sys
 
-#port for the router that the client is connected to
-my_router_port = 22222
 
-def control():
-	print('Need to implement control')
-	return
+if len(sys.argv) >= 1:
+	#port for the router that the client is connected to
+	my_router_port = sys.argv[1]
 
-def send_file(packet):
-	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-	    s.connect(('127.0.0.1', my_router_port))
-	    #tuple, sock_info[0] = ip, sock_info[1] = host
-	    sock_info = s.getsockname()
-	    #s.sendall(str.encode(mes))
-	    #p = packet(HOST+str(PORT), sock_info[1], 0, 'file contents')
-	    encoded_packet = pickle.dumps(packet)
-	    s.sendall(encoded_packet)
-	    data = pickle.loads(s.recv(4096))
-	    if data.contents == 'ack':
-	    	print('Ack received. File transmitted succesfully')
+	def control():
+		print('Need to implement control')
+		return
 
-#ignore this function for now. something like this will need to be implemented on the routers
-#	for periodic alive messages/LSA flooding
+	def send_file(packet):
+		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		    s.connect(('127.0.0.1', my_router_port))
+		    #tuple, sock_info[0] = ip, sock_info[1] = host
+		    sock_info = s.getsockname()
+		    #s.sendall(str.encode(mes))
+		    #p = packet(HOST+str(PORT), sock_info[1], 0, 'file contents')
+		    encoded_packet = pickle.dumps(packet)
+		    s.sendall(encoded_packet)
+		    data = pickle.loads(s.recv(4096))
+		    if data.contents == 'ack':
+		    	print('Ack received. File transmitted succesfully')
 
-print('Welcome to the Client Interface')
-print('Please refer to the numbered commands below')
-print()
-print('Enter 1 for control information')
-print('Enter 2 to send a file')
-print('Enter 3 to view routing table information')
-print('Enter 4 to display shortest paths')
-print('Enter 5 to exit the client')
-print('Enter 6 for help')
-inp = input('')
-if(int(inp) == 1):
-	control()
-elif(int(inp) == 2):
-	#dest = input('Enter dest port (33333): ')
-	p = packet(33333, my_router_port, 0, 'This is a packet')
-	send_file(p)
-#elif(int(inp) == 3):
-	# TODO
-#elif (int(inp) == 4):
-	# TODO
-#elif(int(inp) == 5):
-	# TODO implement exit
-elif (int(inp) == 6):
+	#ignore this function for now. something like this will need to be implemented on the routers
+	#	for periodic alive messages/LSA flooding
+
 	print('Welcome to the Client Interface')
 	print('Please refer to the numbered commands below')
 	print()
@@ -58,5 +38,34 @@ elif (int(inp) == 6):
 	print('Enter 4 to display shortest paths')
 	print('Enter 5 to exit the client')
 	print('Enter 6 for help')
+	inp = input('')
+	if(int(inp) == 1):
+		control()
+	elif(int(inp) == 2):
+		#dest = input('Enter dest port (33333): ')
+		print('Enter the message you wish to send:')
+		msg = input('')
+		print('Enter the port number of the reciever: (33333)')
+		port_num = input('')
+		p = packet(port_num, my_router_port, 0, msg)
+		send_file(p)
+	#elif(int(inp) == 3):
+		# TODO
+	#elif (int(inp) == 4):
+		# TODO
+	#elif(int(inp) == 5):
+		# TODO implement exit
+	elif (int(inp) == 6):
+		print('Welcome to the Client Interface')
+		print('Please refer to the numbered commands below')
+		print()
+		print('Enter 1 for control information')
+		print('Enter 2 to send a file')
+		print('Enter 3 to view routing table information')
+		print('Enter 4 to display shortest paths')
+		print('Enter 5 to exit the client')
+		print('Enter 6 for help')
+	else:
+		print('Invalid Command. Enter 6 for help.')
 else:
-	print('Invalid Command. Enter 6 for help.')
+	print('Syntax should be \'python3 client.py PORT\'')
