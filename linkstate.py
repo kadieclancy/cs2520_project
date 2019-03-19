77aa5e7d0fac304f36fa0c9c0c5ecd318fe1225e
Kadie Clancy <kadieclancy@Kadies-MBP.fios-router.home>
2019-03-19 18:48:37 -0400

    link state obj updates

--- linkstate.py
+++ linkstate.py
@@ -1,5 +1,6 @@
 # linkstate.py
 # contains the Link State info for a local link state
+import numpy as np
 
 class LinkState:
 	def _init_(self, IP):
@@ -10,5 +11,25 @@ class LinkState:
 	def addNeighbor(self, neigh_IP):
 		self.mapping[len(self.mapping)+1] = neigh_IP
 	
-	def updateNeighborDelay(self, neigh_index, delay)
+	def updateNeighborDelay(self, neigh_index, delay):
 		self.mapping[neigh_index] = delay
+	
+	def mapping2Matrix(self):
+		mappingMatrix = np.array([])
+		for key, elem in self.mapping:
+			newrow = np.array([key, elem])
+			mappingMatrix = np.vstack([mappingMatrix, new_row])
+		return mappingMatrix
+		
+	def lsdb2Matrix(self):
+		lsdbMatrix = np.array([])
+		for key, elem in self.LSDB:
+			newrow = np.array([key, elem])
+			lsdbMatrix = np.vstack([lsdbMatrix, new_row])
+		return lsdbMatrix
+	
+	def getMapping(self):
+		return self.mapping
+	
+	def getLSDB(self):
+		return self.LSDB
